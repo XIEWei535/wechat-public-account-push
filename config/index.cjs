9cecac59051cca41f9af7bd4a6932fa75abd9145
XIEWei535 <40680940+XIEWei535@users.noreply.github.com>
2024-03-16 00:38:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,8 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2Wah6YUiUZ6DkIsgf0vq1atVPuc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      //useTemplateId: 'PKMFdjnmQnvCb67BlrnkmStNDojbDYFNlO5HV5T3D1k',
-      useTemplateId: 'vC6OUbOMSWWKq1prAGR4gKO66qnNNngCeWMYUUgG2Wk',
+      useTemplateId: 'PKMFdjnmQnvCb67BlrnkmStNDojbDYFNlO5HV5T3D1k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-09',
       festivals: [
@@ -33,12 +32,12 @@ const USER_CONFIG = {
           type: '*生日', name: '宝贝', year: '1993', date: '10-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1993', date: '10-09',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '08-04',
-        },
+        //  {
+         //   type: '生日', name: '李四', year: '1993', date: '10-09',
+        //  },
+        //  {
+         //   type: '节日', name: '相识纪念日', year: '2022', date: '08-04',
+         // },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
