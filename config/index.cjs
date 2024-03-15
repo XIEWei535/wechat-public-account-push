518012204247c8d85a5c729c9c1b48030f4e0bb6
XIEWei535 <40680940+XIEWei535@users.noreply.github.com>
2024-03-16 00:25:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,8 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2Wah6YUiUZ6DkIsgf0vq1atVPuc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'PKMFdjnmQnvCb67BlrnkmStNDojbDYFNlO5HV5T3D1k',
+      //useTemplateId: 'PKMFdjnmQnvCb67BlrnkmStNDojbDYFNlO5HV5T3D1k',
+      useTemplateId: 'SOLwnIQn8SYSHgyEwOa3jxOXsd9gGx34CoCpBbm6guI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-09',
       festivals: [
