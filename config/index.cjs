e4eac2fc401f6d296c9f8ebce66cc9c558659c98
XIEWei535 <40680940+XIEWei535@users.noreply.github.com>
2024-03-15 23:31:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'SFnaXbYihMZyR2NNz8FKh3BXFuO9e__Kq5J_U567LOA',
+  CALLBACK_TEMPLATE_ID: 'KSXKtnqLTY5IA27cwjlUvZxwFFyFBoiEaJro1VVB2Ic',
 
   CALLBACK_USERS: [
     {
