f49fff87b24030d186c5268a24ccc1571aa8d573
XIEWei535 <40680940+XIEWei535@users.noreply.github.com>
2024-03-15 23:10:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o2Wah6aB9ZQy_hnyLnfmC_n_im3I',
+      id: 'o2Wah6YUiUZ6DkIsgf0vq1atVPuc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'SFnaXbYihMZyR2NNz8FKh3BXFuO9e__Kq5J_U567LOA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
